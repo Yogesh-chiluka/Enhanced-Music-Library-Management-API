6a16179f5ffb808f61eb37c557110f8fe251d4c1
Yogesh Chiluka <yogeshravichiluka@gmail.com>
2024-12-26 17:09:40 +0530

    Create User model - create user model - implement method to hash password before save - implement methods isPasswordCorrect, generateAccessToken, generateRefreshToken - export User

---
+++ src/models/user.model.js
@@ -0,0 +1,71 @@
+import mongoose, { Schema, } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+
+const userSchema = new Schema({
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        index: true
+    },
+    password:{
+        type: String,
+        required: [true,'Password is required']
+    },
+    role:{
+        type: String,
+        enum:['ADMIN','EDITOR','VIEWER'],
+        default: 'VIEWER'
+    },
+    refreshToken:{
+        type: String
+    },
+},
+{
+    timestamps:true
+})
+
+userSchema.pre('save',async function(){
+    if(!this.isModefied("password")) return next();
+    this.password = await bcrypt.hash(this.password,10);
+    next();
+})
+
+userSchema.method.ispasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+
+userSchema.method.generateAccessToken = function(){
+
+    return JsonWebTokenError.sign(
+        {
+            _id:this._id,
+            email:this.email,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+
+userSchema.method.generateRefreshToken = function{
+
+    return JsonWebTokenError.sign(
+        {
+            _id:this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
+export const User = mongoose.model('User',UserSchema);
+

7a451e687a30643e5a944aff177333b371105c8c
Yogesh Chiluka <yogeshravichiluka@gmail.com>
2024-12-26 16:24:26 +0530

    Setup app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,31 @@
 import express from "express";
-
+import cors from "cors";
 
 const app = express();
+
+app.use(cors({
+    oringin:process.env.CROSS_ORIGIN,
+    credentials: true
+}));
+
+app.use(express.json({limit:'16kb'}));
+
+app.use(express.urlencoded({extended:true,limit:'16kb'}));
+
+app.use(express.urlencoded({extended:true, limit:'16kb'}));
+
+app.use(express.static('public'));
+
+app.use(cookieParser());
+
+
+
+
+// Routes Import
+
+
+// Routes Declaration
+
 
 
 
